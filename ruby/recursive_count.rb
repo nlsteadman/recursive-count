81ed17450e3bedb6473c803a616c38c779bc9505
Nicole Steadman <nsteadman@gmail.com>
2022-10-07 12:53:37 -0700

    solved

--- ruby/recursive_count.rb
+++ ruby/recursive_count.rb
@@ -1,6 +1,22 @@
 def recursive_count(num = 0)
-  # type your code in here
+  # count = 0
+
+  # while count < 10
+  #   puts count
+  #   count += 1
+  # end
+
+  
+
+  return if num >= 10
+    puts num
+    num += 1
+  
+  recursive_count(num)
+
 end
+
+puts recursive_count(num = 0)
 
 if __FILE__ == $PROGRAM_NAME
   recursive_count
